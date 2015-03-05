75992319c7316d66dd81d7c9a074445128b2e749
Shrikar Archak <shrikar@viralheat.com>
2015-03-04 22:44:56 -0800

    Initial Commit

---
+++ BridgeApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BridgeApp
+//
+//  Created by Shrikar Archak on 3/4/15.
+//  Copyright (c) 2015 Shrikar Archak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BridgeApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BridgeApp
+//
+//  Created by Shrikar Archak on 3/4/15.
+//  Copyright (c) 2015 Shrikar Archak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
