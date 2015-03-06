1ff01a6bfef8197af8f47158d8c22b0beae5b979
Shrikar Archak <shrikar@viralheat.com>
2015-03-05 23:45:42 -0800

    Initial View controllers in place

---
+++ BridgeApp/CreateJobScene1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateJobScene1ViewController.h
+//  BridgeApp
+//
+//  Created by Shrikar Archak on 3/4/15.
+//  Copyright (c) 2015 Shrikar Archak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateJobScene1ViewController : UIViewController <UITableViewDataSource,UITableViewDelegate>
+
+@end
