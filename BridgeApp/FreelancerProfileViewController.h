ec74848d7a58cc6adceca78d3373e528139a9f01
David Tong <dtong@box.com>
2015-03-06 22:52:33 -0800

    restarting with new Profile View Controller

---
+++ BridgeApp/FreelancerProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  FreelancerProfileViewController.h
+//  BridgeApp
+//
+//  Created by David Tong on 3/6/15.
+//  Copyright (c) 2015 Shrikar Archak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FreelancerProfileViewController : UIViewController
+
+@end
