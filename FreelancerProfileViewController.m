b466c353ffe0bedfdf928c3a63193bdf217ab9c3
David Tong <dtong@box.com>
2015-03-06 22:21:43 -0800

    Revert "Revert "profile view controller setup""
    This reverts commit b91819272fab0c8631ae59e8b00c88f98565b594.

---
+++ FreelancerProfileViewController.m
@@ -0,0 +1,43 @@
+//
+//  FreelancerProfileViewController.m
+//  BridgeApp
+//
+//  Created by David Tong on 3/5/15.
+//  Copyright (c) 2015 Shrikar Archak. All rights reserved.
+//
+
+#import "FreelancerProfileViewController.h"
+
+@interface FreelancerProfileViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *starLabel;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
+@property (weak, nonatomic) IBOutlet UILabel *numOfReviewsLabel;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@end
+
+@implementation FreelancerProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
